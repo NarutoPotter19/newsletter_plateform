efaf271446d1d6f124dae1094a656a725baca8cb
Shivankit Singh <shivankitsingh19@gmail.com>
2024-04-06 22:16:58 +0530

    added provider in layout.tsx and header in Home.tsx
    Now we have added <Providers > to layout.tsx and confined children into it+ Added header component to home.tsz

--- src/modules/home/home.tsx
+++ src/modules/home/home.tsx
@@ -1,9 +1,13 @@
 // import React from 'react'
 
+import Header from "@/shared/widgets/header/header"
+
 const Home = () => {  // here we put Home as capital because the components must 
     // not be start with the small letter .
   return (
     <div>
+      <Header />
+
       
     </div>
   )
